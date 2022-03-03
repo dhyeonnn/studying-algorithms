b009915d7dae72d2d2ceaccd9000121f2d25d834
hyeon <ekgus4074@gmail.com>
2022-03-03 10:28:29 +0900

    10988번: 팰린드롬인지 확인하기

---
+++ BOJ_10988.py
@@ -0,0 +1,19 @@
+# 10988번: 팰린드롬인지 확인하기
+
+pal = input()
+
+if pal==pal[::-1]:
+    print(1)
+else :
+    print('0')
+
+'''word = input()
+
+if word == word[::-1]:
+    print(1)
+else:
+    print(0)
+
+# print(1 if word == word[::-1] else 0)
+# print(int(word == word[::-1]))
+'''
