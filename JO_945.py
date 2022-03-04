01f2c4622ca0f5ef90e55b197d7b2fb34f57fde0
hyeon <ekgus4074@gmail.com>
2022-03-04 14:42:58 +0900

    945 : 기타 자료형 - 자가진단 5

---
+++ JO_945.py
@@ -0,0 +1,17 @@
+# 945 : 기타 자료형 - 자가진단 5
+
+animations = {
+    "Pokemon": "Pikachu",
+    "Digimon": "Agumon",
+    "Yugioh": "Black Magician"
+}
+
+word = input()
+#print(animations[word])
+
+#if word in animations:
+#    print(animations[word])
+#else:
+ #   print("I don't know")
+
+print(animations.get(word, "I don't know"))
